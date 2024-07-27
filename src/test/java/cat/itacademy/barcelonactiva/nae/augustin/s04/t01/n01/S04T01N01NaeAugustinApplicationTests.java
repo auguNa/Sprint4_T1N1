5956f45c8542e3da21d0636781df780a3148531f
auguNa <augustin.nae@gmail.com>
2024-07-27 11:14:19 +0200

    first commit

---
+++ src/test/java/cat/itacademy/barcelonactiva/nae/augustin/s04/t01/n01/S04T01N01NaeAugustinApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.nae.augustin.s04.t01.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01NaeAugustinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
